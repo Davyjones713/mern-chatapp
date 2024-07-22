ea21bc43610c1e9340a220220bf5de5e43402dcd
davyjones713 <datoiashvili713@gmail.com>
2024-07-22 13:38:18 +0400

    2nd part completed - ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+// import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
